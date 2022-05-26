d218dc178ab6998032a929657f08519c3cad6a6d
Juan J Guirao Garcia <juan.jose.guirao@gmail.com>
2022-05-26 13:31:02 +0100

    All done until beginning of Ex 3.15

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const { response } = require("express");
 const Note = require("./models/note");
-
-require("dotenv").config();
 
 const requestLogger = (request, response, next) => {
     console.log("Method:", request.method);
@@ -34,20 +33,21 @@ const generateId = () => {
 app.post("/api/notes", (request, response) => {
     const body = request.body;
 
-    if (!body.content) {
+    if (body.content === undefined) {
         return response.status(400).json({
             error: "content missing",
         });
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
-        id: generateId(),
-    };
+    });
 
-    notes = notes.concat(note);
+    note.save().then(savedNote => {
+        response.json(savedNote);
+    });
 
     response.json(note);
 });
@@ -59,21 +59,32 @@ app.get("/api/notes", (req, res) => {
 });
 
 app.delete("/api/notes/:id", (request, response) => {
-    const id = Number(request.params.id);
-    notes = notes.filter(note => note.id !== id);
-
-    response.status(204).end();
+    Note.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end();
+        })
+        .catch(error => next(error));
 });
 
 app.get("/api/notes/:id", (request, response) => {
-    const id = Number(request.params.id);
-    const note = notes.find(note => note.id === id);
-
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note);
-    } else {
-        response.status(404).end();
-    }
+    });
+});
+
+app.put("/api/notes/:id", (request, response, next) => {
+    const body = request.body;
+
+    const note = {
+        content: body.content,
+        important: body.important,
+    };
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote);
+        })
+        .catch(error => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
@@ -82,7 +93,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
