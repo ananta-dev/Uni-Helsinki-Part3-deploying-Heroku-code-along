e29299041619b1a49801d030bc7df765b039727a
Juanjo Guirao <juan.jose.guirao@gmail.com>
2022-05-26 13:00:51 +0100

    Working on creating separate model for mongoose logic

--- backend/index.js
+++ backend/index.js
@@ -1,49 +1,10 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mongoose = require("mongoose");
 const { response } = require("express");
+const Note = require("./models/note");
 
 require("dotenv").config();
-
-mongoose.connect(process.env.MONGODB_URL);
-
-const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
-});
-
-noteSchema.set("toJSON", {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    },
-});
-
-const Note = mongoose.model("Note", noteSchema);
-
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        date: "2022-01-10T17:30:31.098Z",
-        important: true,
-    },
-    {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2022-01-10T18:39:34.091Z",
-        important: false,
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2022-01-10T19:20:14.298Z",
-        important: true,
-    },
-];
 
 const requestLogger = (request, response, next) => {
     console.log("Method:", request.method);
